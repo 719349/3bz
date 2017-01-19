006f8eb27006ae7fc7351dd8fa98087f6fa433e8
719349 <UP719349@myport.ac.uk>
2017-01-19 19:31:28 +0000

    Login page
    UI prototype of the login page.

---
+++ public/javascripts/login.js
@@ -0,0 +1,23 @@
+let setUp = function () {
+	let form = document.getElementById('loginForm');
+	form.addEventListener('submit', function(e){
+		e.preventDefault();
+		let auth = {user: 'adeola', password: 'jollof'};
+		let formElements = e.target.getElementsByTagName('input');
+		let status = document.getElementById('loginStatus');
+
+		if (formElements[0].value === auth.user
+			&& formElements[1].value === auth.password
+		) {
+			status.innerText = 'Valid user';
+			status.classList.add('valid');
+			status.classList.remove('invalid');
+		} else {
+			status.innerText = 'User password combination not recognised';
+			status.classList.add('invalid');
+			status.classList.remove('valid');
+		}
+	});
+};
+
+window.addEventListener('load', setUp);

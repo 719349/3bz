a10792d7c1325e195064951fccf576a9208a22d3
719349 <UP719349@myport.ac.uk>
2017-02-06 18:09:56 +0000

    Updated order page
    Added sidebar
    Orders saved to local storage when button pressed
    Added lubricants to page with styling.

--- public/javascripts/productSelection.js
+++ public/javascripts/productSelection.js
@@ -1,19 +1,41 @@
-let setUp = function(){
+let
+retriveOrder = function() {
+	order = localStorage.order;
+	if (order) {
+		return JSON.parse(order)
+	} else {
+		return {};
+	}
+},
+
+saveOrder = function(order) {
+	localStorage.order = JSON.stringify(order)
+},
+
+setUp = function(){
 	document.addEventListener('change', function(e) {
 		let costElement = e.target.form.getElementsByClassName('cost')[0];
 		costElement.innerText = costElement.dataset.cost * e.target.value;
-		getGotalCosts();
-	})
+	});
+
+	document.addEventListener('submit', function(e) {
+		e.preventDefault();
+		let costElement = e.target.getElementsByClassName('cost')[0];
+		let productName = costElement.dataset.product;
+		let productOrder = Number.parseFloat(costElement.innerText);
+		let order = retriveOrder();
+		order[productName] = productOrder;
+		saveOrder(order);
+		getTotalCosts();
+	});
+	getTotalCosts();
 },
 
-getGotalCosts = function() {
-	let individualCosts = document.getElementsByClassName('cost');
+getTotalCosts = function() {
 	let totalCost = 0;
-	for (var itemIndex in individualCosts) {
-		if (individualCosts.hasOwnProperty(itemIndex)) {
-			let thisCost = Number.parseFloat(individualCosts[itemIndex].innerText);
-			totalCost += thisCost;
-		}
+	let order = retriveOrder();
+	for (var product in order) {
+		totalCost += order[product];
 	}
 	document.getElementById('totalCost').innerText = totalCost;
 	return totalCost;

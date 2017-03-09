aa4b73edc8afa621b225ed673c521b0e3562d5a7
719349 <UP719349@myport.ac.uk>
2017-03-09 15:54:57 +0000

    Only process images when the element exists
    Fails safely and silently if no element

--- public/javascripts/lightbox.js
+++ public/javascripts/lightbox.js
@@ -1,15 +1,17 @@
 let startLightbox = function() {
 	let img = document.getElementById('lightbox');
-	let images = [
-		'lb3.jpg',
-		'lb5.jpg'
-	];
-	let i = 0;
-	let changeImage = function() {
-		img.src = '../images/' + images[i];
-		i = (i + 1) < images.length ? i + 1 : 0;
-	};
-	setInterval(changeImage, 5000);
+	if (img) {
+		let images = [
+			'lb3.jpg',
+			'lb5.jpg'
+		];
+		let i = 0;
+		let changeImage = function() {
+			img.src = '../images/' + images[i];
+			i = (i + 1) < images.length ? i + 1 : 0;
+		};
+		setInterval(changeImage, 5000);
+	}
 };
 
 window.addEventListener('load', startLightbox);

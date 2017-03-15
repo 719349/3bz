d0a7ad8eb9eaa426af3e6b254d0eea0bc92a983d
719349 <UP719349@myport.ac.uk>
2017-03-15 17:46:10 +0000

    Added logout
    Logout functionality  added

--- public/javascripts/menubar.js
+++ public/javascripts/menubar.js
@@ -1,10 +1,22 @@
 let updateMenu = function() {
 	let user = sessionStorage.getItem('user');
+	let link = document.getElementById('user');
+	let logout = document.getElementById('logout');
+
+	logout.addEventListener('click', function () {
+		sessionStorage.removeItem('user');
+		link.innerText = 'My account';
+		link.href = '../pages/user';
+		logout.style.display = 'none';
+	});
 
 	if (user !== null) {
-		let link = document.getElementById('user');
 		link.innerText = 'Hello ' + user + ' :)';
 		link.href = '../pages/user';
+		logout.style.display = 'inline-block';
+	}
+	else {
+		logout.style.display = 'none';
 	}
 };
 
